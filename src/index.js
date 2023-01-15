239e8c7cdedc339b6e4948559e4999328cc70567
Javier Aceves <jaacevesh@gmail.com>
2023-01-14 21:39:44 -0600

    first commit, dependency done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+
+const greet = name => 'Hello' + name;
+
+const PI = 3.1415;
+
+const users = async () => {
+   const res = await axios.get('https://jsonplaceholder.typicode.com/users?_limit=2') 
+   return res.data;
+}
+
+module.exports = {
+    users,
+    greet,
+    PI
+}
