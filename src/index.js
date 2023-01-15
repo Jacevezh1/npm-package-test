e2cac2d68e6b2fcd91d5ec9b1d38a685d9173477
Javier Aceves <jaacevesh@gmail.com>
2023-01-14 21:47:01 -0600

    testing branch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const greet = name => 'Hello' + name;
 
-const PI = 3.1415;
+const PI = 3.14155454554;
 
 const users = async () => {
    const res = await axios.get('https://jsonplaceholder.typicode.com/users?_limit=2') 
